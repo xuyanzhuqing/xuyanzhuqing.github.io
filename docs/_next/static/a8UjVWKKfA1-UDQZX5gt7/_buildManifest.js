fc13360b4b06f975f4af7e8fc982d67fa22940d4
michael <962275976@qq.com>
2023-12-13 13:48:22 +0800

    build docs

--- docs/_next/static/a8UjVWKKfA1-UDQZX5gt7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-09ba1f8b5aa3fcc8.js"],"/_error":["static/chunks/pages/_error-f02d05701ff074c5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
