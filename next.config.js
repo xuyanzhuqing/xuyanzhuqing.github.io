cdee9802b4fa81938e7de8a8f5605e200f68f117
michael <962275976@qq.com>
2023-12-13 10:29:20 +0800

    update pkg message

--- next.config.js
+++ next.config.js
@@ -5,13 +5,12 @@ const nextConfig = {
   output: 'export',
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
+  trailingSlash: true,
 
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
   // skipTrailingSlashRedirect: true,
 
   // Optional: Change the output directory `out` -> `dist`
-  distDir: 'docs',
+  distDir: process.env.NODE_ENV === 'production' ? 'docs' : '.next',
 }
-
 module.exports = nextConfig
