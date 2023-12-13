c32aa77d121306b23058af6ee678c0876f678f9e
michael <962275976@qq.com>
2023-12-13 17:36:53 +0800

    修复style attribute

--- components/Layout.js
+++ components/Layout.js
@@ -31,7 +31,7 @@ function LayoutPage({ children }) {
     <div className="isolate bg-white px-6 py-24 sm:py-8 lg:px-8">
       <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]" aria-hidden="true">
         <div className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-          style={{ "clip-path": "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }}
+          style={{ "clipPath": "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }}
         ></div>
       </div>
       <nav className="mx-auto flex max-w-7xl items-center justify-center p-6 lg:px-8" aria-label="Global">
@@ -149,7 +149,9 @@ function LayoutPage({ children }) {
           </div>
         </Dialog.Panel>
       </Dialog>
-      {children}
+      <div className='mx-auto max-w-7xl items-center p-6 lg:px-8'>
+        {children}
+      </div>
     </div>
   )
 }
