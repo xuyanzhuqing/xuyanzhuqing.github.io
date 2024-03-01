0007ef66f3bcfd5e19918b20ca94320c2b53daf2
michael <962275976@qq.com>
2024-03-01 14:54:07 +0800

    代码优化

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import '/styles/globals.css'
 import Layout from '/components/layout'
 
 export const metadata = {
-  title: 'Michael\'s blog',
+  title: 'xuyanzhuqing',
   description: 'just record',
 }
 
@@ -13,13 +13,13 @@ export default function RootLayout({ children }) {
         <Layout>
           {children}
         </Layout>
-        <div class="website-report">
-          <p class="website-desc">Designed & Powerd by xuyanzhuqing</p>
-          <a class="website-report-link" href="http://beian.miit.gov.cn" target="_blank">
-            <img width='20px' src="//img.alicdn.com/tfs/TB1..50QpXXXXX7XpXXXXXXXXXX-40-40.png" class="ali-report-img" />
-            <span class="website-report-link-text">沪ICP备2024052162号</span>
+        <div className="website-report">
+          <p className="website-desc">Designed & Powerd by xuyanzhuqing</p>
+          <a className="website-report-link" href="http://beian.miit.gov.cn" target="_blank">
+            <img width='20px' src="//img.alicdn.com/tfs/TB1..50QpXXXXX7XpXXXXXXXXXX-40-40.png" />
+            <span className="website-report-link-text">沪ICP备2024052162号</span>
           </a>
-          <p class="website-desc">Copyright© 2024 xuyanzhuqing blog</p>
+          <p className="website-desc">Copyright© 2024 xuyanzhuqing blog</p>
         </div>
       </body>
     </html>
