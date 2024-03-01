04ca91572e74935baaf5f9c661390d192cd9a33c
michael <962275976@qq.com>
2024-03-01 14:31:40 +0800

    新增域名备案信息

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,14 @@ export default function RootLayout({ children }) {
         <Layout>
           {children}
         </Layout>
+        <div class="website-report">
+          <p class="website-desc">Designed & Powerd by xuyanzhuqing</p>
+          <a class="website-report-link" href="http://beian.miit.gov.cn" target="_blank">
+            <img width='20px' src="//img.alicdn.com/tfs/TB1..50QpXXXXX7XpXXXXXXXXXX-40-40.png" class="ali-report-img" />
+            <span class="website-report-link-text">沪ICP备2024052162号</span>
+          </a>
+          <p class="website-desc">Copyright© 2024 xuyanzhuqing blog</p>
+        </div>
       </body>
     </html>
   )
